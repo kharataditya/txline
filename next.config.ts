7e9194b9fba0701cd3c292662a06e519c6d40236
User <user@example.com>
2026-07-20 02:54:08 +0530

    fix: next config strict type error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
   // Turbopack is default in Next.js 16
   turbopack: {},
   // Allow external images if needed
